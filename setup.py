1d9726a40d5ff3b6f0f1d3f5382f775b28a27adf
Quentin Eude <quentin.eude@ripplemotion.fr>
2020-03-28 13:25:17 +0100

    🚀 Updated on PyPi

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='csvtotrello',
-    version='0.1',
+    version='0.3.0',
     description='A package to bulk import csv backlog into Trello',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/qeude/csvtotrello',
     author='Quentin Eude',
     author_email='quentineude@gmail.com',
